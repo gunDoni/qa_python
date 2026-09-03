6348d1f6a388610b95068bc2af2209747174024d
Arsen Vartanov <arsen10012004@gmail.com>
2026-09-03 21:17:58 +0300

    2-3 are done

--- tests.py
+++ tests.py
@@ -18,7 +18,25 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+# -----------------------------------------------------------------------------------------------------------------------------------
+# 2ой тест для метода set_book_genre с существующим жанром
+    def test_set_book_genre_add_existing_genre(self):
+        collector_2 = BooksCollector()
+        collector_2.add_new_book('Шерлок Холмс')
+        collector_2.set_book_genre('Шерлок Холмс', 'Детективы')
+        assert collector_2.get_book_genre('Шерлок Холмс') == 'Детективы'
+
+# -----------------------------------------------------------------------------------------------------------------------------------
+# 3ий тест для того же метода set_book_genre с несуществующим жанром
+    def test_set_book_genre_add_nonexisting_genre(self):
+        collector_3 = BooksCollector()
+        collector_3.add_new_book('Я - легенда')
+        collector_3.set_book_genre('Я - легенда', 'Романы')
+        assert not collector_3.get_book_genre('Я - легенда') == 'Романы'
+
+# -----------------------------------------------------------------------------------------------------------------------------------
